d857806dc58c99bd64ecee7a1791b6ee3ef9969f
siddharthwork3382 <siddharthwork3382@gmail.com>
2024-01-24 18:19:28 +0530

    Running Project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,11 @@
 package org.example;
 
-import org.example.Coffee.BlackCoffee;
 import org.example.Coffee.Coffee;
+import org.example.Coffee.Latte;
 
 public class Main {
     public static void main(String[] args) {
-    Coffee coffee =new BlackCoffee();
+    Coffee coffee =new Latte();
     coffee.makeCoffee();
 
     }
